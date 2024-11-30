012471092ea40b2de3ef00978ffbccf02f9f7704
Santinal Browns <60613674+santinalbrowns@users.noreply.github.com>
2024-12-01 00:23:19 +0200

    update

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,5 +1,25 @@
 import type { PageServerLoad } from './$types';
+import { auth } from '$lib/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { redirect } from '@sveltejs/kit';
 
 export const load = (async () => {
     return {};
 }) satisfies PageServerLoad;
+
+export const actions = {
+    default: async ({ request }) => {
+
+        const data = await request.FormData();
+        const email = data.get("email").toString();
+        const password = data.get("password").toString();
+
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+            throw redirect(302, "/tickes");
+        } catch (error) {
+            console.error('Error logging in:', error.message);
+        }
+    }
+}
