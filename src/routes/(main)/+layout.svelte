5c4f6181a576bbf1b4ea81db92074d29bbedaf26
Santinal Browns <60613674+santinalbrowns@users.noreply.github.com>
2024-11-30 23:55:33 +0200

    Init

---
+++ src/routes/(main)/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+    import type { LayoutData } from './$types';
+
+    let { data, children }: { data: LayoutData, children: Snippet } = $props();
+</script>
+
+<header>
+    <h1>E-Ticketing and Attendance Tracking System</h1>
+    <nav>
+        <img src="Logo.jpg" alt="logo">
+        <a href="Homee.html">Home</a>
+        <a href="login.html">Login</a>
+        <a href="register.html">Register</a>
+        <a href="Match.html">Match</a>
+        <a href="confirmation.html">Confirmation</a>
+    </nav>
+</header>
+
+{@render children()}
---
+++ src/routes/(main)/confirm/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+
+    let { data }: { data: PageData } = $props();
+</script>
+
+<main>
+    <h2>Thank you for your purchase!</h2>
+    <h3>Ticket Details</h3>
+    <p>Match: Civo United vs Bangwe All Stars</p>
+    <p>Date: 2024-09-22</p>
+    <p>Time: 14:30</p>
+    <p>Venue: Civo Stadium</p>
+    <p>Category: Premium</p>
+    <p>Quantity: 2</p>
+    <img src="qrcode.png" alt="QR Code">
+</main>
